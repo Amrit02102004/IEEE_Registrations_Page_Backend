d0db4fc0215bcaec4ed1b889347c2cc740418638
Amrit Sundarka <114827768+Amrit02102004@users.noreply.github.com>
2024-03-11 13:38:42 +0530

    port change for frontend

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'http://127.0.0.1:5500', 
+    origin: 'http://127.0.0.1:5173', 
 };
 
 app.use(cors(corsOptions));
