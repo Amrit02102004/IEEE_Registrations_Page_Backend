63cf5c44f3f9456f801f9b1ead7a9b54107d5376
Amrit Sundarka <amritsundarka@gmail.com>
2024-03-16 00:07:28 +0530

    Checking Cors

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: 
-  ['http://127.0.0.1:5173','http://127.0.0.1:5500'],
+  ['http://127.0.0.1:5173'
+  // ,'http://127.0.0.1:5500'
+],
 };
 
 app.use(cors(corsOptions));
